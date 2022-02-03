9d80eb13db367d672e5a4db0b5865ab141e3e55d
Song Joo <43398106+AllenEdgarPoe@users.noreply.github.com>
2022-02-03 11:39:40 +0900

    Update download_vg.sh

--- scripts/download_vg.sh
+++ scripts/download_vg.sh
@@ -25,6 +25,7 @@ wget https://visualgenome.org/static/data/dataset/relationship_alias.txt -O $VG_
 wget https://visualgenome.org/static/data/dataset/image_data.json.zip -O $VG_DIR/image_data.json.zip
 wget https://cs.stanford.edu/people/rak248/VG_100K_2/images.zip -O $VG_DIR/images.zip
 wget https://cs.stanford.edu/people/rak248/VG_100K_2/images2.zip -O $VG_DIR/images2.zip
+wget https://github.com/google/sg2im/raw/master/sg2im/data/vg_splits.json -O $VG_DIR/vg_splits.json
 
 unzip $VG_DIR/objects.json.zip -d $VG_DIR
 unzip $VG_DIR/attributes.json.zip -d $VG_DIR
