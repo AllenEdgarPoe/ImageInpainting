5948e2797562e1a818aa5230004f5afcfb0e58b5
Song Joo <43398106+AllenEdgarPoe@users.noreply.github.com>
2022-02-03 11:17:57 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -3,6 +3,7 @@ MODEL=$1
 if [ $MODEL == "generative-attention" ]; then
     cd generative-inpainting
     python train.py --config '../configs/generative-config.yaml' --psnr True
+fi
 
 if [ $MODEL == "edge-connect" ]; then
     cd edge-connect
